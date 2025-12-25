f3190fc9ec40be653405656d70e95790b42ce041
nescondo <nescondo2@gmail.com>
2025-12-24 20:09:34 -0500

    added blinking animation, styled header

--- src/components/ui/top-navbar.tsx
+++ src/components/ui/top-navbar.tsx
@@ -11,25 +11,27 @@ import { BsGithub, BsLinkedin, BsYoutube } from "react-icons/bs";
 function TopNavBar() {
     return (
         <>
-            <div className="grid grid-cols-5 p-8">
-                <h2 className="col-start-1 m-auto text-2xl">NE</h2>
-                <NavigationMenu className="col-start-3 m-auto">
+            <div className="grid grid-cols-3 p-8">
+                <div className="col-start-1 m-auto text-2xl">
+                    <h2 className="">&lt; Nikolai Escondo /&gt; <a className="animate-cursor">|</a></h2>
+                </div>
+                <NavigationMenu className="col-start-2 m-auto">
                     <NavigationMenuList>
                         <NavigationMenuItem>
                             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                <a href="#" className="text-xl">Home</a>
+                                <a href="#" className="text-lg">&lt; Home /&gt;</a>
                             </NavigationMenuLink>
                             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                <a href="#about-me" className="text-xl">About Me</a>
+                                <a href="#about-me" className="text-lg">&lt; About Me /&gt;</a>
                             </NavigationMenuLink>
                             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                <a href="#projects" className="text-xl">Projects</a>
+                                <a href="#projects" className="text-lg">&lt; Projects /&gt;</a>
                             </NavigationMenuLink>
                         </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu>
 
-                <NavigationMenu className="col-start-5 m-auto scale-200">
+                <NavigationMenu className="col-start-3 m-auto scale-200">
                     <NavigationMenuLink>
                         <Link href="https://github.com/nescondo">
                             <BsGithub />
